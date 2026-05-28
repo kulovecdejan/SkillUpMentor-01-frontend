6fe6242342d9bc475f192ea1cd7665dd834fa1fd
Dejan Kulovec <kulovecdejan@gmail.com>
2026-05-28 15:56:56 +0200

    Add Tailwind CSS

--- src/pages/landing/LoginPage.tsx
+++ src/pages/landing/LoginPage.tsx
@@ -1,6 +1,13 @@
+import '../../index.css'
+
 const LoginPage = () => {
     return (
-        <div> Hello on the login page </div>
+        <>
+            <div> Hello on the login page </div>
+            <h1 className="text-5xl bg-amber-300 font-bold underline">
+            Hello world!
+            </h1>
+        </>
     )
 }
 
